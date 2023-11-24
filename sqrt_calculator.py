5a9f8cf44bf76bbc7d5467a6319217d810a26c91
Ab-integral <spagsoo@gmail.com>
2023-11-24 16:22:20 +0100

    fixing

--- sqrt_calculator.py
+++ sqrt_calculator.py
@@ -1,12 +1,12 @@
 from math import sqrt
 # Taking input from the user
 
-number = int(input("Enter a number: "))
+num = int(input("Please enter a number: "))
 
 # Calculating square root
 
-sqrt_calculator = sqrt(number)
+sqrt_calculator = sqrt(num)
 
 # Displaying the result
 
-print(f"Square root of the number {number} is = {sqrt_calculator}")
+print(f"Square root of the number {num} is = {sqrt_calculator}")

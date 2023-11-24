3295c88822de2089e1d70a71ced8acb03ea4baec
Ab-integral <spagsoo@gmail.com>
2023-11-24 16:19:56 +0100

    completed my first task

---
+++ sqrt_calculator.py
@@ -0,0 +1,12 @@
+from math import sqrt
+# Taking input from the user
+
+number = int(input("Enter a number: "))
+
+# Calculating square root
+
+sqrt_calculator = sqrt(number)
+
+# Displaying the result
+
+print(f"Square root of the number {number} is = {sqrt_calculator}")
